2db794e12f6e427206944feec5b22228231fe062
Thiago Lopes <thiago.lopes.dev@gmail.com>
2020-03-08 19:54:14 -0300

    Adicionando componentes e hooks

--- App-With-TypeScript/client/src/App.tsx
+++ App-With-TypeScript/client/src/App.tsx
@@ -1,8 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import api from './services/api';
+import User from './components/User';
+
+interface IUser {
+    name: string;
+    email: string;
+}
 
 function App(){
+    const [users, setUsers] = useState<IUser[]>([]);
+    useEffect(() =>{
+        api.get<IUser[]>('/users')
+            .then(response => {
+                setUsers(response.data);
+            })
+    }, [])
     return (
-        <div></div>
+        <div>
+            {users.map(user => <p><User key={user.email} user={user}/></p>)}
+        </div>
     );
 }
 
